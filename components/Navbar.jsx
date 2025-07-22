ac7c987b4eb0649a039c87220d93548188894b43
ShyaAyad <shyaayad1@gmail.com>
2025-07-22 15:36:27 +0300

    added the sidebar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Avatar, Typography } from "antd";
-import image from "/public/note-image.svg";
-import { HomeOutlined, UserAddOutlined } from "@ant-design/icons";
+import { HomeOutlined, UserOutlined } from "@ant-design/icons";
+import logo from "../images/note-logo.jpg"
 
 const {Title} = Typography;
 
@@ -8,15 +8,15 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="logo">
-        <Avatar size="large" src={image} style={{ margin: "15px" }} />
+        <Avatar size="large" src={logo}  style={{ margin: "15px" }} />
         <Title level={2} style={{ color: "white" }}>
           Notes
         </Title>
       </div>
 
       <div className="nav-icons">
-        <Title level={3}><HomeOutlined className="home-icon"/></Title>
-        <Title level={3}><UserAddOutlined className="user-icon"/></Title>
+        <Title level={4} className="icon"><HomeOutlined className="home-icon"/>Home</Title>
+        <Title level={4} className="icon"><UserOutlined className="user-icon"/>Account</Title>
       </div>
       
     </div>
